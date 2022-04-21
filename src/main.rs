5fbdb31843a355b72a6b19b7ecd7b00cba83fadf
KoyaSaeki <koyasaeki@gmail.com>
2022-04-21 23:59:38 +0900

    update: 引数の値を変数に保存する

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("ファイル {} の中にある", filename);
+    println!("{} を探す", query);
 }
