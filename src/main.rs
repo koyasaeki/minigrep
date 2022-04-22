00b7d385d25a481b651fb2c0a6fa7de12722a06d
KoyaSaeki <koyasaeki@gmail.com>
2022-04-22 18:07:19 +0900

    update: 引数パーサー関数を作る

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ use std::io::Read;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("ファイル {} の中にある", filename);
-    println!("{} を探す", query);
+    let (query, filename) = parse_config(&args);
 
     let mut f = File::open(filename).expect("file not found");
 
@@ -18,4 +13,11 @@ fn main() {
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &Vec<String>) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
