0239ad3cb1070fd24753ecdbfd0cd5aa3d5cef6b
KoyaSaeki <koyasaeki@gmail.com>
2022-04-22 17:56:31 +0900

    update: poem ファイルを読み込めるようにする

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -8,4 +10,12 @@ fn main() {
 
     println!("ファイル {} の中にある", filename);
     println!("{} を探す", query);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
